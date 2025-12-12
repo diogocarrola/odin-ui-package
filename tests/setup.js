48669410072a47b8299787bcabf98da055503273
Diogo Carrola <diogo.carrola@gmail.com>
2025-12-12 17:25:20 +0000

    Add Jest test setup with DOM mock and whatwg-fetch polyfill

---
+++ tests/setup.js
@@ -0,0 +1,8 @@
+import 'whatwg-fetch';
+
+// Setup basic DOM for testing
+global.document.body.innerHTML = `
+    <div id="test-dropdown"></div>
+    <div id="test-carousel"></div>
+    <form id="test-form"></form>
+`;
