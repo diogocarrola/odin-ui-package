875f5f36a0ed862299c25eb61f66c1580443b4e1
Diogo Carrola <diogo.carrola@gmail.com>
2025-12-12 17:15:13 +0000

    Add Jest configuration for testing with jsdom environment

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.demo.js',
+    '!src/index.js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
+};
