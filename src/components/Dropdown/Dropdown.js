b9ccbc47fe41c1f3c9563b87c11e560e79bdb276
Diogo Carrola <diogo.carrola@gmail.com>
2025-12-13 23:53:30 +0000

    Create Dropdown component with basic structure and options

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,36 @@
+export class Dropdown {
+  constructor(element, options = {}) {
+    this.element = element;
+    this.options = {
+      trigger: options.trigger || 'click',
+      closeOnClickOutside: options.closeOnClickOutside !== false,
+      animation: options.animation !== false,
+      ...options
+    };
+    
+    this.isOpen = false;
+    this.init();
+  }
+  
+  init() {
+    console.log('Dropdown initialized - implement me!');
+  }
+  
+  open() {
+    console.log('Opening dropdown - implement me!');
+  }
+  
+  close() {
+    console.log('Closing dropdown - implement me!');
+  }
+  
+  toggle() {
+    console.log('Toggling dropdown - implement me!');
+  }
+  
+  destroy() {
+    console.log('Cleaning up dropdown - implement me!');
+  }
+}
+
+export { Dropdown };
